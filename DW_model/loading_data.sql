01b24954e4477280349580700b84c6d4ce4f499a
basel nabil <basel.nabil1111@gmail.com>
2024-08-06 16:35:59 +0300

    created denormallized table , and loaded data into it

---
+++ DW_model/loading_data.sql
@@ -0,0 +1,6 @@
+LOAD DATA LOCAL INFILE '/home/basel/basel/etl_project/data/raw/crash_data.csv'
+INTO TABLE crash_reports``
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
