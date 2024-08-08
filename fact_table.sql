90aac72a3017efc9eaedf2e5dd0ebcf090b3db74
alialkady <66703864+alialkady@users.noreply.github.com>
2024-08-08 14:29:34 +0300

    Add files via upload

---
+++ fact_table.sql
@@ -0,0 +1,19 @@
+USE data_warehouse;
+-- Fact Table: crash_fact
+CREATE TABLE crash_fact (
+    [Report Number] VARCHAR(70),
+    [Person ID] VARCHAR(70),
+    [Vehicle ID] VARCHAR(70),
+    [Latitude] VARCHAR(70),
+    [Longitude] VARCHAR(70),
+    [Location] VARCHAR(70),
+	[date_id]varchar(70),
+    PRIMARY KEY ([Report Number]),
+    FOREIGN KEY ([Report Number]) REFERENCES report_description([Report Number]),
+    FOREIGN KEY ([Person ID]) REFERENCES driver_description([Person ID]),
+    FOREIGN KEY ([Vehicle ID]) REFERENCES vehicle_description([Vehicle ID]),
+	FOREIGN KEY ([date_id]) REFERENCES date_description([date_id] )
+	)
+drop table crash_fact
+
+
