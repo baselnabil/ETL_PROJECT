6740375ae499897c220cee6d16d11571407ef569
alialkady <66703864+alialkady@users.noreply.github.com>
2024-08-14 20:26:55 +0300

    Add files via upload

--- dimensions.sql
+++ dimensions.sql
@@ -1,66 +1,66 @@
 CREATE DATABASE data_warehouse;
-
+use master
 drop database data_warehouse
 USE data_warehouse;
 
 select * from vehicle_description
 -- Dimension Table: vehicle_description
 CREATE TABLE vehicle_description (
-	[Number] int identity(1,1) primary key,
-    [Vehicle ID] VARCHAR(70) ,
-    [Vehicle Damage Extent] VARCHAR(70),
-    [Vehicle First Impact Location] VARCHAR(70),
-    [Vehicle Second Impact Location] VARCHAR(70),
-    [Vehicle Body Type] VARCHAR(70),
-    [Vehicle Movement] VARCHAR(70),
-    [Vehicle Continuing Dir] VARCHAR(70),
-    [Vehicle Going Dir] VARCHAR(70),
-    [Speed Limit] VARCHAR(70),
-    [Driverless Vehicle] VARCHAR(70),
-    [Parked Vehicle] VARCHAR(70),
-    [Vehicle Year] VARCHAR(70),
-    [Vehicle Make] VARCHAR(70),
-    [Vehicle Model] VARCHAR(70),
-    [Equipment Problems] VARCHAR(70),
-    [Latitude] VARCHAR(70),
-    [Longitude] VARCHAR(70),
-    [Location] VARCHAR(70),
-    [Route Type] VARCHAR(70),
-    [Road Name] VARCHAR(70),
-    [Cross-Street Type] VARCHAR(70),
-    [Cross-Street Name] VARCHAR(70),
-    [Off-Road Description] VARCHAR(70),
-    [Municipality] VARCHAR(70),
-    [Related Non-Motorist] VARCHAR(70),
-    [Collision Type] VARCHAR(70),
-    [Weather] VARCHAR(70),
-    [Surface Condition] VARCHAR(70),
-    [Light] VARCHAR(70),
-    [Traffic Control] VARCHAR(70)
-);
+		[Number] int identity(1,1) primary key,
+		[Vehicle ID] VARCHAR(70) ,
+		[Vehicle Damage Extent] VARCHAR(70),
+		[Vehicle First Impact Location] VARCHAR(70),
+		[Vehicle Second Impact Location] VARCHAR(70),
+		[Vehicle Body Type] VARCHAR(70),
+		[Vehicle Movement] VARCHAR(70),
+		[Vehicle Continuing Dir] VARCHAR(70),
+		[Vehicle Going Dir] VARCHAR(70),
+		[Speed Limit] VARCHAR(70),
+		[Driverless Vehicle] VARCHAR(70),
+		[Parked Vehicle] VARCHAR(70),
+		[Vehicle Year] VARCHAR(70),
+		[Vehicle Make] VARCHAR(70),
+		[Vehicle Model] VARCHAR(70),
+		[Equipment Problems] VARCHAR(70),
+		[Latitude] VARCHAR(70),
+		[Longitude] VARCHAR(70),
+		[Location] VARCHAR(70),
+		[Route Type] VARCHAR(70),
+		[Road Name] VARCHAR(70),
+		[Cross-Street Type] VARCHAR(70),
+		[Cross-Street Name] VARCHAR(70),
+		[Off-Road Description] VARCHAR(70),
+		[Municipality] VARCHAR(70),
+		[Related Non-Motorist] VARCHAR(70),
+		[Collision Type] VARCHAR(70),
+		[Weather] VARCHAR(70),
+		[Surface Condition] VARCHAR(70),
+		[Light] VARCHAR(70),
+		[Traffic Control] VARCHAR(70)
+	);
 
--- Dimension Table: report_description
-CREATE TABLE report_description (
-	[Number] int identity(1,1) primary key,
-    [Report Number] VARCHAR(70),
-    [Local Case Number] VARCHAR(70),
-    [Agency Name] VARCHAR(70),
-    [ACRS Report Type] VARCHAR(70),
-    [Route Type] VARCHAR(70),
-    [Road Name] VARCHAR(70),
-    [Cross-Street Type] VARCHAR(70),
-    [Cross-Street Name] VARCHAR(70),
-    [Off-Road Description] VARCHAR(70),
-    [Municipality] VARCHAR(70),
-    [Related Non-Motorist] VARCHAR(70),
-    [Collision Type] VARCHAR(70),
-    [Weather] VARCHAR(70),
-    [Surface Condition] VARCHAR(70),
-    [Light] VARCHAR(70),
-    [Traffic Control] VARCHAR(70),
-    [Driver Substance Abuse] VARCHAR(70),
-    [Non-Motorist Substance Abuse] VARCHAR(70)
-);
+	-- Dimension Table: report_description
+	CREATE TABLE report_description (
+		[Number] int identity(1,1) primary key,
+		[Report Number] VARCHAR(70),
+		[Local Case Number] VARCHAR(70),
+		[Agency Name] VARCHAR(70),
+		[ACRS Report Type] VARCHAR(70),
+		[Route Type] VARCHAR(70),
+		[Road Name] VARCHAR(70),
+		[Cross-Street Type] VARCHAR(70),
+		[Cross-Street Name] VARCHAR(70),
+		[Off-Road Description] VARCHAR(70),
+		[Municipality] VARCHAR(70),
+		[Related Non-Motorist] VARCHAR(70),
+		[Collision Type] VARCHAR(70),
+		[Weather] VARCHAR(70),
+		[Surface Condition] VARCHAR(70),
+		[Light] VARCHAR(70),
+		[Traffic Control] VARCHAR(70),
+		[Driver Substance Abuse] VARCHAR(70),
+		[Non-Motorist Substance Abuse] VARCHAR(70)
+	);
 
 -- Dimension Table: driver_description
 CREATE TABLE driver_description (
--- fact_table.sql
+++ fact_table.sql
@@ -1,19 +1,23 @@
 USE data_warehouse;
 -- Fact Table: crash_fact
 CREATE TABLE crash_fact (
-    [Report Number] VARCHAR(70),
-    [Person ID] VARCHAR(70),
-    [Vehicle ID] VARCHAR(70),
-    [Latitude] VARCHAR(70),
-    [Longitude] VARCHAR(70),
-    [Location] VARCHAR(70),
-	[date_id]varchar(70),
+    [Report Number] int,
+    [Person ID] int,
+    [Vehicle ID] int,
+    [Latitude] int,
+    [Longitude] int,
+    [Location] int,
+	[date_id]int,
     PRIMARY KEY ([Report Number]),
-    FOREIGN KEY ([Report Number]) REFERENCES report_description([Report Number]),
-    FOREIGN KEY ([Person ID]) REFERENCES driver_description([Person ID]),
-    FOREIGN KEY ([Vehicle ID]) REFERENCES vehicle_description([Vehicle ID]),
-	FOREIGN KEY ([date_id]) REFERENCES date_description([date_id] )
+    FOREIGN KEY ([Report Number]) REFERENCES report_description([Number]),
+    FOREIGN KEY ([Person ID]) REFERENCES driver_description([Number]),
+    FOREIGN KEY ([Vehicle ID]) REFERENCES vehicle_description([Number]),
+	FOREIGN KEY ([date_id]) REFERENCES date_description([Number] )
 	)
 drop table crash_fact
+
+drop database data_warehouse
+
+select * from crash_fact
 
 
